5b01b54175c9257b975e3a4fe5df9ff3f946edec
Sven de Goede <72500744+Raz-rD@users.noreply.github.com>
2020-11-12 11:47:11 +0100

    Files for challenge 1

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+
+    // Start is called before the first frame updatesss
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + new Vector3(85, 3, -1);
+    }
+}
---
+++ Assets/Challenge 1/Scripts/Spinnin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinnin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed = 2;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, speed);
+    }
+}
